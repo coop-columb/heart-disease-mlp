915c75e46160518d965cf6443897be4dc8ac28f5
coop-columb <ahc2188@columbia.edu>
2025-03-21 20:07:18 -0400

    chore(release): bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="heart-disease-mlp",
-    version="0.1",
+    version="1.0.0",
     packages=find_packages(),
 )
